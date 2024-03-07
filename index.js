c2d311d524ed2bb91d43252097390b451c883a8b
biskyy <54770799+biskyy@users.noreply.github.com>
2024-03-07 23:47:53 +0200

    Refactoring + login request

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 const Song = require("./models/songModel");
+const songSchema = require("./schemas/songSchema");
+const adminUsername = process.env.ADMIN_USERNAME;
+const adminPassword = process.env.ADMIN_PASSWORD;
 const privateKey = process.env.PRIVATE_KEY;
 
 const sortPipeline = [
@@ -55,63 +59,102 @@ mongoose
     console.log(error);
   });
 
+const formatSongTitle = (songTitle) => {
+  return songTitle
+    .normalize("NFKD")
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s.-]+/g, " ");
+};
+
+const formatSongContent = (songContent) => {
+  return songContent
+    .normalize("NFKD")
+    .toLowerCase()
+    .replace(/[^\w\s-]|[\d]/gm, "")
+    .replace(/[\s.-]+/g, " ")
+    .replace(/ refren | r /g, " ")
+    .trim();
+};
+
 app.get("/songs", async (req, res) => {
   try {
     const songs = await Song.find({});
     res.status(200).json(songs);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(501).json({ message: error.message });
   }
 });
 
-app.get("/song/:bookID/:id", async (req, res) => {
+app.get("/song/:book_id/:id", async (req, res) => {
   try {
-    const { bookID, id } = req.params;
-    const song = await Song.findOne({ book_id: bookID, id: id }).exec();
+    const { book_id, id } = req.params;
+    const song = await Song.findOne({ book_id: book_id, id: id }).exec();
     res.status(200).json(song);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: error.message });
   }
 });
 
 app.post("/song", authenticate, async (req, res) => {
   try {
     await Song.create(req.body);
-    try {
-      await sortDatabase();
-      res.status(200).send("The song got uploaded successfully");
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+    await sortDatabase();
+    res.status(200).send("The song got uploaded successfully");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.put("/song/:bookID/:id", authenticate, async (req, res) => {
+app.put("/song", authenticate, async (req, res) => {
   try {
-    const { bookID, id } = req.params;
-    const song = await Song.findOneAndReplace(
-      { book_id: bookID, id: id },
+    await songSchema.validateAsync(req.body);
+  } catch (validationError) {
+    return res.status(400).json({ message: validationError.message });
+  }
+
+  try {
+    const { book_id, id } = req.body;
+
+    req.body.searchable_title = formatSongTitle(req.body.title);
+    req.body.searchable_content = formatSongContent(req.body.content);
+
+    const replacedSong = await Song.findOneAndReplace(
+      { book_id, id },
       req.body
     );
 
-    if (!song)
-      return res
-        .status(404)
-        .send(`Can't find the song with book_id: "${bookID}" and id: ${id}`);
+    if (!replacedSong)
+      return res.status(404).json({
+        message: `Can't find the song with book_id: "${book_id}" and id: ${id}`,
+      });
 
-    try {
-      await sortDatabase();
-      res.status(200).send("The song got uploaded successfully");
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+    await sortDatabase();
 
-    const updatedSong = Song.findOne({ book_id: bookID, id: id }).exec();
-
-    res.status(200).json(updatedSong);
+    res.status(200).json({ message: "The song was updated successfully" });
   } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/login", (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (username !== adminUsername)
+      res.status(404).json({ message: "Incorrect username" });
+    else if (!bcrypt.compareSync(password, adminPassword))
+      res.status(404).json({ message: "Incorrect password" });
+    else {
+      const token = jwt.sign(
+        { username: adminUsername, password: adminPassword },
+        privateKey
+      );
+
+      res.status(200).json({ message: "Logged in", token });
+    }
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ message: error.message });
   }
 });
