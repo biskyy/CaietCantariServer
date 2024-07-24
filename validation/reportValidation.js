d89a0cb3816582163bcc32c597c1f05c2fd95f97
biskyy <54770799+biskyy@users.noreply.github.com>
2024-07-24 11:55:26 +0300

    make additionalDetails optional

--- validation/reportValidation.js
+++ validation/reportValidation.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const songSchema = Joi.object({
   songIndex: Joi.number().required(),
-  additionalDetails: Joi.string(),
+  additionalDetails: Joi.string().allow(""),
 });
 
 module.exports = songSchema;
