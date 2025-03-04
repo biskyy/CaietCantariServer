f1fac7a40738b2b12a7ef38e6cb5bc9e8968a609
biskyy <andreisirbu07@gmail.com>
2025-03-04 11:57:16 +0200

    refactor: rewrite the rate limiting system
    - implement rate limiting based on ip or jwt token
    - create a new middleware for rate limiting individual routes
    independent of what client accessed that route;

--- middleware/limiter.js
+++ middleware/limiter.js
@@ -1,5 +1,7 @@
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
 import Client from "../models/clientModel.js";
+import Route from "../models/routeModel.js";
 
 const getIp = (req) => {
   const forwardedFor = req.headers["x-forwarded-for"];
@@ -10,64 +12,177 @@ const getIp = (req) => {
   return null;
 };
 
-const hashIp = (ip, secret) => {
-  return crypto.createHmac("sha256", secret).update(ip).digest("hex");
+const hashIdentifier = (identifier, secret) => {
+  return crypto.createHmac("sha256", secret).update(identifier).digest("hex");
 };
 
-const rateLimitByIp = (limit = 1, window = 10000) => {
+export const rateLimitRoute = (limit = 5, window = 30000) => {
   return async (req, res, next) => {
     try {
-      // Skip rate-limiting in development
-      if (process.env.NODE_ENV === "DEV") return next();
+      if (process.env.NODE_DEV === "DEV") return next();
 
-      const ip = getIp(req);
       const method = req.method;
-      const route = req.baseUrl;
+      const routePath = req.baseUrl;
+      const route = method + routePath;
 
-      if (!ip) return res.status(400).json({ message: "IP Address not found" });
-
-      const secret = process.env.RATE_LIMIT_SECRET;
-      if (!secret) {
-        console.error("RATE_LIMIT_SECRET is not set.");
-        return res.status(500).json({ message: "Server misconfiguration" });
-      }
-
-      const hashedIp = hashIp(ip, secret);
-
-      let client = (await Client.findOne({ ip: hashedIp }).exec()) || {
-        ip: hashedIp,
-        trackers: {},
-      };
-
-      let tracker = client.trackers[method + route] || {
+      const routeTracker = (await Route.findOne({
+        route: route,
+      }).exec()) || {
+        route: method + routePath,
         count: 0,
         expiresAt: 0,
       };
 
-      // Reset the tracker if the window has expired
-      if (tracker.expiresAt < Date.now()) {
-        tracker.count = 0;
-        tracker.expiresAt = Date.now() + window;
+      // tracker has expired reset counter
+      if (routeTracker.expiresAt < Date.now()) {
+        routeTracker.count = 0;
+        routeTracker.expiresAt = Date.now() + window;
       }
 
-      tracker.count++;
+      routeTracker.count++;
 
-      client.trackers[method + route] = tracker;
+      if (routeTracker.count > limit) {
+        throw new Error("Rate limit exceeded");
+      }
 
-      if (tracker.count > limit) {
+      await Route.findOneAndUpdate({ route: route }, routeTracker, {
+        upsert: true,
+      });
+
+      return next();
+    } catch (err) {
+      if (err.message === "Rate limit exceeded") {
         return res.status(429).json({
           message: "You have to wait a little bit between requests.",
         });
       }
 
-      await Client.findOneAndUpdate({ ip: hashedIp }, client, { upsert: true });
-
-      return next();
-    } catch (err) {
       console.error("Rate limit middleware error: ", err);
       return res.status(500).json({ message: "Internal server error" });
     }
   };
 };
 
-export default rateLimitByIp;
+const rateLimitByIdentifier = async (
+  identifier,
+  method,
+  route,
+  limit,
+  window,
+) => {
+  if (!identifier) {
+    throw new Error("Identifier not provided");
+  }
+
+  const secret = process.env.RATE_LIMIT_SECRET;
+  if (!secret) {
+    throw new Error("Rate limit secret not configured");
+  }
+
+  let client = (await Client.findOne({ identifier }).exec()) || {
+    identifier,
+    trackers: {},
+  };
+
+  let tracker = client.trackers[method + route] || {
+    count: 0,
+    expiresAt: 0,
+  };
+
+  // Reset the tracker if the window has expired
+  if (tracker.expiresAt < Date.now()) {
+    tracker.count = 0;
+    tracker.expiresAt = Date.now() + window;
+  }
+
+  tracker.count++;
+
+  client.trackers[method + route] = tracker;
+
+  if (tracker.count > limit) {
+    throw new Error("Rate limit exceeded");
+  }
+
+  await Client.findOneAndUpdate({ identifier }, client, {
+    upsert: true,
+  });
+};
+
+const rateLimitByIp = (limit = 1, window = 10000) => {
+  return async (req, res, next) => {
+    try {
+      if (process.env.NODE_ENV === "DEV") return next();
+
+      const ip = getIp(req);
+      const method = req.method;
+      const route = req.baseUrl;
+
+      const hashedIp = hashIdentifier(ip, secret);
+
+      await rateLimitByIdentifier(hashedIp, method, route, limit, window);
+
+      return next();
+    } catch (err) {
+      if (err.message === "Rate limit exceeded") {
+        return res.status(429).json({
+          message: "You have to wait a little bit between requests.",
+        });
+      }
+
+      console.error("Rate limit middleware error: ", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
+};
+
+export const rateLimitByJWT = (limit, window) => {
+  return async (req, res, next) => {
+    // default arguments dont work because this is a function that returns a middleware and express doesnt like that?
+    limit = limit ?? 2;
+    window = window ?? 10000;
+
+    try {
+      if (process.env.NODE_ENV === "DEV") return next();
+
+      const token = req.headers.authorization?.split(" ")[1];
+      const method = req.method;
+      const route = req.baseUrl;
+
+      if (!token) {
+        return res.status(400).json({ message: "JWT token not provided" });
+      }
+
+      const privateKey = process.env.PRIVATE_KEY;
+      if (!privateKey) {
+        throw new Error("PRIVATE_KEY not set up");
+      }
+
+      let decoded;
+      try {
+        let payload = jwt.verify(token, privateKey);
+        decoded = payload;
+      } catch (err) {
+        return res.status(400).json({ message: "Invalid JWT token" });
+      }
+      //jwt.verify(token, privateKey, (err, payload) => {
+      //  console.log("reached");
+      //  // if the jwt is either expired or forged(bad signature) discard it
+      //  decoded = payload;
+      //});
+      //console.log(decoded);
+
+      await rateLimitByIdentifier(decoded.uuid, method, route, limit, window);
+
+      return next();
+    } catch (err) {
+      if (err.message === "Rate limit exceeded") {
+        return res.status(429).json({
+          message: "You have to wait a little bit between requests.",
+        });
+      }
+
+      console.error("Rate limit middleware error: ", err);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  };
+};
