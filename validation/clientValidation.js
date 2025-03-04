9506545ba69a989edbe47fb67ba649df72c0265b
biskyy <andreisirbu07@gmail.com>
2025-03-04 12:08:51 +0200

    fix: renamed property from ip to identifier

--- validation/clientValidation.js
+++ validation/clientValidation.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 const clientSchema = Joi.object({
-  ip: Joi.string().required(),
+  identifier: Joi.string().required(),
   //count: Joi.number().required(),
   //expiresAt: Joi.date().required(),
   trackers: Joi.object(),
